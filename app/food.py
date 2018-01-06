f358b008ff269e2672a4e338752dbc910dbed656
Michael Lynch <michael@isitketo.org>
2018-01-06 14:47:21 -0500

    Setting up boilerplate

--- app/food.py
+++ app/food.py
@@ -1,9 +1,9 @@
 from google.appengine.ext import ndb
 
+
 class Food(ndb.Model):
     title = ndb.StringProperty()
     title_lower = ndb.ComputedProperty(lambda self: self.title.lower())
     image_url = ndb.StringProperty()
     description = ndb.StringProperty()
     rating = ndb.IntegerProperty()
-
