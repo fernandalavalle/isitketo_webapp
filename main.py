ae3f1be7968ec7744f53fa307791c9b518200120
Michael Lynch <michael@isitketo.org>
2018-01-06 13:55:35 -0500

    Adding basic datastore functionality

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import logging
 
-from flask import Flask
+import flask
+from google.appengine.ext import ndb
+
+import food
+
+app = flask.Flask(__name__)
 
 
-app = Flask(__name__)
+def find_food(food_name):
+    key = ndb.Key(food.Food, food_name)
+    return key.get()
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def root():
+    return 'Welcome to isitketo! Search to find out if a food is keto-friendly.'
+
+
+@app.route('/<food_name>')
+def check_food(food_name):
+    f = find_food(food_name)
+    return '%s: %s' % (f.title, f.description)
 
 
 @app.errorhandler(500)
 def server_error(e):
-    # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
