90a2abf4e9761801e155b8a41334c7ba8a5f6efc
Michael Lynch <michael@isitketo.org>
2018-01-12 22:46:06 -0500

    Adding a short_description to Food
    Removing image_url because we find image URL by key

--- app/food.py
+++ app/food.py
@@ -4,6 +4,6 @@ from google.appengine.ext import ndb
 class Food(ndb.Model):
     title = ndb.StringProperty()
     title_lower = ndb.ComputedProperty(lambda self: self.title.lower())
-    image_url = ndb.StringProperty()
     description = ndb.StringProperty()
+    short_description = ndb.StringProperty()
     rating = ndb.IntegerProperty()
