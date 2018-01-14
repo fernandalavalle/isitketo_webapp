ad91e5c6c5b12b5261b8dde08726c7e2e9a482e4
Michael Lynch <michael@isitketo.org>
2018-01-14 15:24:42 -0500

    Removing stray debug line

--- app/auth_manager.py
+++ app/auth_manager.py
@@ -6,7 +6,6 @@ def is_request_authorized(request):
     # Hacky implementation until we do real auth.
     token = request.form.get('token')
     digest = hashlib.pbkdf2_hmac('sha256', token, b'pretzel house', 10000)
-    print 'result=[%s]' % binascii.hexlify(digest)
     return binascii.hexlify(
         digest
     ) == b'79f7a50742e7dfad41993b5edf3b1954246efc52b0f46c711bcc65cdd3ed8d9c'
