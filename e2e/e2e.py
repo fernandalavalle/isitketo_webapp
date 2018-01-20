44718f9f67b1549738ced0a8442813c2698ad298
Michael Lynch <michael@isitketo.org>
2018-01-19 21:03:31 -0500

    Updating e2e script

--- e2e/e2e.py
+++ e2e/e2e.py
@@ -1,4 +1,5 @@
 import argparse
+import contextlib
 import logging
 
 from selenium import webdriver
@@ -10,31 +11,46 @@ logger = logging.getLogger(__name__)
 def main(args):
     _configure_logging()
 
-    logger.info('starting remote driver at %s', args.selenium_hub_url)
-    driver = webdriver.Remote(
-        command_executor=args.selenium_hub_url,
-        desired_capabilities={
-            'browserName': 'chrome',
-            'javascriptEnabled': True
-        })
-    logger.info('loading app: %s', args.app_url)
-    driver.get(args.app_url)
+    with contextlib.closing(_load_browser(args.selenium_hub_url)) as browser:
+        _run_e2e_flow(browser, args.app_url)
 
-    logger.info('Page Title: [%s]', driver.title)
-    driver.save_screenshot('main-page.png')
+
+def _run_e2e_flow(browser, app_url):
+    logger.info('loading app: %s', app_url)
+    browser.get(app_url)
+
+    logger.info('Page Title: [%s]', browser.title)
+    browser.save_screenshot('main-page.png')
 
     logger.info('finding query element')
-    elem = driver.find_element_by_id('query')
+    elem = browser.find_element_by_id('query')
     elem.send_keys('diet coke')
     elem.send_keys(keys.Keys.RETURN)
-    driver.save_screenshot('diet-coke.png')
+    browser.save_screenshot('diet-coke.png')
 
-    elem = driver.find_element_by_id('query')
+    elem = browser.find_element_by_id('query')
     elem.send_keys('sushi')
     elem.send_keys(keys.Keys.RETURN)
-    driver.save_screenshot('sushi.png')
+    browser.save_screenshot('sushi.png')
 
-    driver.close()
+
+def _load_browser(selenium_hub_url):
+    logger.info('loading remote webdriver at %s', selenium_hub_url)
+    attempts = 0
+    MAX_ATTEMPTS = 10
+    while True:
+        try:
+            return webdriver.Remote(
+                command_executor=selenium_hub_url,
+                desired_capabilities={
+                    'browserName': 'chrome',
+                    'javascriptEnabled': True
+                })
+        except Exception as e:
+            if attempts < MAX_ATTEMPTS:
+                attempts += 1
+                continue
+            raise
 
 
 def _configure_logging():
